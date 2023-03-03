43f0c70be358c8c2f6508db249aedaebd5c1ccf4
deinde dawodu <ddawodu@brightsign.biz>
2023-03-03 17:59:06 -0500

    adding in correct vue version of jsversion

---
+++ vue-project-2/src/App.vue
@@ -0,0 +1,40 @@
+
+<script setup>
+//THIS IS STATE!!!!
+import {ref} from "vue"
+const count = ref(0)
+
+const addToCount = ()=>{
+  console.log('add')
+  count.value = count.value +1
+}
+const subtractCount = ()=>{
+  console.log('sub')
+  if(count.value >=1){
+    count.value = count.value - 1
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractCount">-</button>
+      <button @click="addToCount">+</button>
+    </div>
+  </main>
+</template>
+<style scoped>
+ main{
+  width:100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+ }
+ div{
+  text-align: center;
+ }
+</style>
