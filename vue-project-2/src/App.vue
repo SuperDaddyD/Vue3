bc557c501cb4c35f6bdc00099672cf3fc05c3a3b
deinde dawodu <ddawodu@brightsign.biz>
2023-03-04 20:56:10 -0500

    addig in more logic and directives v-model, v-if, v-show, obj.value :key, :style, ref

--- vue-project-2/src/App.vue
+++ vue-project-2/src/App.vue
@@ -1,67 +1,155 @@
 
+
 <script setup>
-//THIS IS STATE!!!!
-//Composition Api STYLE!!!
-import {ref} from "vue"
-const count = ref(0)
+import {ref} from "vue";
+const showModal = ref(false);
+const newNote = ref("");
+const notes = ref([]);
+const errorMssg = ref("");
 
-const addToCount = ()=>{
-  console.log('add')
-  count.value = count.value +1
+function getRandomColor(){
+  return "hsl("+ Math.random() * 360 +", 100%, 75%)";
 }
-const subtractCount = ()=>{
-  console.log('sub')
-  if(count.value >=1){
-    count.value = count.value - 1
-  }
+
+const addNote =()=>{
+  if(newNote.value.length <= 10){
+    return  errorMssg.value = "Sorry. Message must be at least 10 characters"
+
+  } 
+  console.log("Add Note!")
+  
+  notes.value.push({
+   id:Math.floor(Math.random()* 1000000000787),
+   text:newNote.value,
+   date:new Date(),
+   backgroundColor:getRandomColor()
+  })
+
+  showModal.value = false;
+
+  newNote.value = "";
+  errorMssg.value = ""
+
 }
+
+
 </script>
-
-
-<!-- <script>
-//OPTIONS API STYLE!!!
-export default({
-  data(){
-    return{
-      count:0
-    }
-  },
-  methods:{
-    addToCount(){
-      console.log("Hello Add")
-      this.count = this.count +1
-      console.log(this.count)
-     
-    },
-    subtractCount(){
-      console.log("Hello Add")
-      this.count = this.count - 1
-      console.log(this.count)
-     
-    }
-  }
-})
-</script> -->
 
 <template>
   <main>
-    <div>
-      <h4>The current count is...</h4>
-      <h1>{{ count }}</h1>
-      <button @click="subtractCount">-</button>
-      <button @click="addToCount">+</button>
+    <div v-if="showModal" class="overlay">
+     <div class="modal">
+       
+       <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10" ></textarea>
+       <p v-if="errorMssg">{{ errorMssg }}</p>
+
+      <button @click="addNote">Add Note</button>
+
+      <button @click="showModal=false" class="close">Close</button>
+     </div>
+    </div>
+
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button @click="showModal =true ">+</button>
+      </header>
+        <div class="cards-container">
+          <div v-for="note in notes"  :key="note.id" class="card" :style="{backgroundColor:note.backgroundColor}">
+            <p class="main-text">{{ note.text }}</p>
+            <p class="date">{{note.date.toLocaleDateString("en-US")}}</p>
+          </div>
+          
+        </div>
     </div>
   </main>
 </template>
+
 <style scoped>
- main{
+main{
+  height:100vh;
   width:100vw;
-  height: 100vh;
+}
+.container{
+  max-width: 1000px;
+  padding:10px;
+  margin:0 auto;
+}
+header{
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
- }
- div{
-  text-align: center;
- }
+}
+h1{
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+header button{
+  border:none;
+  cursor: pointer;
+  cursor: pointer;
+  background-color: black;
+  color: white;
+  border-radius:50%;
+  font-size: 20px;
+  width: 50px;
+  height: 50px
+}
+.cards-container{
+  display: flex;
+  flex-wrap: wrap;
+}
+.card{
+  width:225px;
+  height: 225px;
+  background: rgb(237, 182, 44);
+  border-radius: 15px;
+  padding:10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-right: 20px;
+  margin-bottom: 20px ;
+}
+
+.date{
+  font-size:12.5px;
+  font-weight: bold;
+}
+.overlay{
+  position: absolute;
+  width: 100vw;
+  height:100vh;
+  z-index: 10;
+  background: rgba(0,0,0,.77 );
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.modal{
+  width:750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+.modal button{
+  padding:10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color:white;
+  margin-top: 15px;
+}
+.modal .close{
+  background-color: red;
+  margin-top: 7px;
+}
+.modal p{
+  color:red;
+}
 </style>
