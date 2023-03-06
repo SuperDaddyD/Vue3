0cbc2a6174c7a465a79afdee0c7f3fc18d39cf83
deinde dawodu <ddawodu@brightsign.biz>
2023-03-06 18:00:39 -0500

    adding home button to end of quiz

--- vue-project-3/src/components/Result.vue
+++ vue-project-3/src/components/Result.vue
@@ -1,11 +1,15 @@
 <script setup>
 import {defineProps} from "vue"
+import { RouterLink } from "vue-router";
 const {numberOfCorrectAnswers,quizQuestion} = defineProps(["numberOfCorrectAnswers","quizQuestion"])
 </script>
 <template>
   <div class="results">
     <p>Your Results...</p>
     <h1>{{numberOfCorrectAnswers}}/{{quizQuestion }}</h1>
+    <RouterLink to="/">
+      <button>Home Quizes</button>
+    </RouterLink>
   </div>
 </template>
 <style scoped>
@@ -18,5 +22,10 @@ p{
 }
 h1 {
   font-size: 58px;
+}
+button{
+    padding:10px;
+
+    font-size: 20px;
 }
 </style>
